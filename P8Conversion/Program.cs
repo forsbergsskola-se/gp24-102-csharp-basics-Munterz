bcaf4cf72f555598e69883b541e30f593b594229
Munterz <munterskevin@hotmail.com>
2024-09-17 11:29:47 +0200

    P8Conversion Works

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Give me a number");
+string input = Console.ReadLine();
+
+float floatValue = float.Parse(input);
+Console.WriteLine(floatValue);
+
+int directIntValue = int.Parse(input);
+Console.WriteLine(directIntValue);
+
