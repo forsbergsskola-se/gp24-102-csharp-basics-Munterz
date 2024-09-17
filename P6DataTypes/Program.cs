e05e377349dcf17ed0567a0bf224677ae28cde2f
Munterz <munterskevin@hotmail.com>
2024-09-17 09:31:55 +0200

    P6DataTypes works
    Typing different types

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,8 @@
+﻿string outputString = "Hello World";
+Console.WriteLine(outputString);
+
+float decimalValue = 0.3f;
+Console.WriteLine(decimalValue);
+
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
